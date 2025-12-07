e55a2f1e8f1ba2d529c2c9eb32fdecb6d38e7414
Gabriel Abubakar <gabrielogrima@gmail.com>
2025-12-07 12:25:17 +0100

    feat: eslint and prettier setup

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
-import app from "./server.ts";
+import app from './server.ts';
 
 app.listen(process.env.PORT || 8000, () => {
+    // eslint-disable-next-line no-console
     console.log(`Server running on port ${process.env.PORT || 8000}`);
 });
--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,19 @@
-import express, { Request, Response } from "express";
-import morgan from "morgan";
-import helmet from "helmet";
-import dotenv from "dotenv";
+import express, { Request, Response } from 'express';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import dotenv from 'dotenv';
 
 dotenv.config();
 const app = express();
 
 // Middlewares
 app.use(express.json());
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 app.use(helmet({ crossOriginResourcePolicy: false }));
 
-app.get("/", (req: Request, res: Response) => {
-    res.send("Hello World!");
-})
+app.get('/', (req: Request, res: Response) => {
+    console.log('sdfa');
+    res.send('sdfa');
+});
 
 export default app;
